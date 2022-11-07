f1ffc661fbb01b6831b78881ecb3288ba437f4ee
Azuby <haivip123vp@gmail.com>
2022-11-07 22:48:07 +0700

    dht22

--- Core/Inc/DHT22.h
+++ Core/Inc/DHT22.h
@@ -3,8 +3,19 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f1xx_hal.h"
+#include "stdint.h"
 
 uint16_t getTemp();
 uint16_t getHumidity();
+
+class DHTHandler {
+    public:
+        void DHTInit();
+        void DHTStart();
+        uint16_t DHTGetTemp();
+        uint16_t DHTGetHumidity();
+        void DHTStop();
+    private:
+};
 
 #endif
